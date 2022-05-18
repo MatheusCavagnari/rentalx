2209b4bc2b228f92e5f1256a6c17bf9fc5ac0a7b
unknown <matheuscavagnari77@gmail.com>
2022-05-18 19:20:40 -0300

    refatoração das especificações

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,12 +1,17 @@
+import { inject, injectable } from "tsyringe";
+
 import { ISpecificationsRepository } from "../../repositories/ISpecificatiosRepository";
 
 interface IRequest {
   name: string;
   description: string;
 }
-
+@injectable()
 class CreateSpecificationUseCase {
-  constructor(private specificationsRepository: ISpecificationsRepository) { }
+  constructor(
+    @inject("SpecificationRepository")
+    private specificationsRepository: ISpecificationsRepository // eslint-disable-next-line prettier/prettier
+  ) { }
   execute({ name, description }: IRequest): void {
     const specificationAlreadyExists =
       this.specificationsRepository.findByName(name);
---
+++ src/shared/container/index.ts
@@ -0,0 +1,16 @@
+import { container } from "tsyringe";
+
+import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
+import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
+import { SpecificationRepository } from "../../modules/cars/repositories/implementations/SpecificationsRespository";
+import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificatiosRepository";
+
+container.registerSingleton<ICategoriesRepository>(
+  "CategoriesRepository",
+  CategoriesRepository
+);
+
+container.registerSingleton<ISpecificationsRepository>(
+  "SpecificationRepository",
+  SpecificationRepository
+);
